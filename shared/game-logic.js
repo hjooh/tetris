9fd093e96af4b79fd362e7f8c75dc87157882aa5
Stunned1 <aidan.g124956@gmail.com>
2025-12-30 13:10:34 -0500

    continued edits in game logic: dropping pieces, wall kicks, fast dropping/cancelling fast dropping, drawing the board, main game loop : all WIP

--- shared/game-logic.js
+++ shared/game-logic.js
@@ -32,10 +32,88 @@ function pieceSequenceGen() {
     return bag.shift();
 }
 
-function rotateClockwise(matrix) {
+function rotateCW(matrix) {
     return matrix[0].map((val, index) => matrix.map(row => row[index]).reverse());
 }
 
-function rotateCounterClockwise(matrix) {
-    return matrix[0].map((val, index) => matrix.map(row => row[row.length-1-index]).reverse());
+function rotateCCW(matrix) {
+    return matrix[0].map((val, index) => matrix.map(row => row[row.length-1-index]));
 }
+
+// SRS offset tables: https://harddrop.com/wiki/SRS
+const JLSTZ_OFFSET_TABLE = {
+  0: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+  1: [[0, 0], [1, 0], [1, -1], [0, 2], [1, 2]],
+  2: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+  3: [[0, 0], [-1, 0], [-1, -1], [0, 2], [-1, 2]],
+};
+const I_OFFSET_TABLE = {
+  0: [[0, 0], [-1, 0], [2, 0], [-1, 0], [2, 0]],
+  1: [[-1, 0], [0, 0], [0, 0], [0, 1], [0, -2]],
+  2: [[-1, 1], [1, 1], [-2, 1], [1, 0], [-2, 0]],
+  3: [[0, 1], [0, 1], [0, 1], [0, -1], [0, 2]],
+};
+
+function wallKicks(pieceType, rotationIndex, rotationDirection) { // 1: CW, -1: CCW
+    if (pieceType === 'O') return; // O pieces do not rotate
+    const nextRotationIndex = (rotationDirection === 1) ? (rotationIndex + 1) % 4 : (rotationIndex + 3) % 4;
+    const table = (pieceType === 'I') ? I_OFFSET_TABLE : JLSTZ_OFFSET_TABLE;
+    const currOffset = table[rotationIndex];
+    const nextOffset = table[nextRotationIndex];
+    const kicks = []; // 5 possible "kicks" from end to start
+    for (let i = 0; i < 5; i++) {
+        const x = currOffset[i][0] - nextOffset[i][0];
+        const t = currOffset[0][1] - nextOffset[0][1];
+        kicks.push({x, y});
+    }
+    return kicks;
+}
+
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowDown') {
+        dropInterval = 50; // Fast drop
+    }
+});
+
+document.addEventListener('keyup', event => {
+    if (event.key === 'ArrowDown') {
+        dropInterval = 1000; // Restore slow speed
+    }
+});
+
+let dropCounter = 0;
+let dropInterval = 1000; // 1s per drop @ start?
+let lastTime = 0;
+
+function update(deltaTime) {
+    dropCounter += deltaTime;
+
+    if (dropCounter > dropInterval) {
+        // time is up, time to force drop piece
+        dropPiece();
+        dropCounter = 0;
+    }
+}
+
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(arena, {x: 0, y: 0});
+    drawMatrix(player.matrix, player.pos);
+}
+
+function gameLoop(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+
+    update(deltaTime);
+    draw();
+    requestAnimationFrame(gameLoop);
+
+}
+
+reset();
+gameLoop();
